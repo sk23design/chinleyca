c22e7e4141e126ecf2d96aa4a9e1c02a121bf58c
Martin Toner <martin.toner@sk23.co.uk>
2022-03-21 18:02:54 +0000

    Routes

--- routes/web.php
+++ routes/web.php
@@ -18,18 +18,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $page = \App\Models\Page::find(1)->first();
+
     return view(
         'welcome',
         [
-        'venues' => \App\Models\Venue::all(),
-        'events' => \App\Models\Event::all(),
+        'venues' => \App\Models\Venue::take(3)->get(),
+        'events' => \App\Models\Event::take(3)->get(),
         'villages' => \App\Models\Village::all(),
+        'page' => $page,
         ]
     );
 });
 
 Route::get('/venues', function () {
-    return view('venues', ['venues' => \App\Models\Venue::all()]);
+    $page = \App\Models\Page::where('slug', 'venues')->first();
+
+    return view('venues', ['venues' => \App\Models\Venue::all(), 'page' => $page]);
 });
 
 Route::get('/venue/{venue:slug}', function (Venue $venue) {
@@ -37,7 +42,9 @@ Route::get('/venue/{venue:slug}', function (Venue $venue) {
 });
 
 Route::get('/events', function () {
-    return view('events', ['events' => Event::all()]);
+    $page = \App\Models\Page::where('slug', 'activities')->first();
+
+    return view('events', ['events' => Event::all(), 'page' => $page]);
 });
 
 Route::get('/event/{event:slug}', function (Event $event) {
@@ -45,7 +52,9 @@ Route::get('/event/{event:slug}', function (Event $event) {
 });
 
 Route::get('/activities', function () {
-    return view('events', ['events' => Event::all()]);
+    $page = \App\Models\Page::where('slug', 'activities')->first();
+
+    return view('events', ['events' => Event::all(), 'page' => $page]);
 });
 
 Route::get('/activity/{event:slug}', function (Event $event) {
@@ -55,7 +64,6 @@ Route::get('/activity/{event:slug}', function (Event $event) {
 Route::get('/search', function (Request $request) {
     $venues = Venue::search($request->search)->get()->toArray();
     $events = Event::search($request->search)->get()->toArray();
-
     $results = [];
     $results['venues'] = $venues;
     $results['events'] = $events;
@@ -71,10 +79,34 @@ Route::get('/information/{information:slug}', function (Information $information
     return view('information', ['information' => $information]);
 });
 
-Route::get('/about-us', function () {
-    return view('about');
+Route::get('/about', function () {
+    $page = \App\Models\Page::where('slug', 'about')->first();
+
+    return view('page', ['page' => $page]);
 });
 
 Route::get('/contact', function () {
-    return view('contact');
+    $page = \App\Models\Page::where('slug', 'contact')->first();
+
+    return view('page', ['page' => $page]);
+});
+
+Route::get('/test', function () {
+    /*
+
+    FB_SECRET_KEY=31028bebf86d3785561d4982795fb9b5
+FB_APP_ID=134406219963629
+FB_ACCESS_TOKEN_OLD=EAAB6PeLZCHO0BAI2H7JBXpitGY1wjFmAPZCApcPURHsqHM03CoXDX9QHzARJAZCbQDe3ZBzfkbAAkXTkFM34in5wAHXc9blt3ffwu2twEkPDCTj7KgQJP6Aet80OjgTgmZBsDg3ZCNNHQAmA5jPrh6KDc53NlDkdVI8u9mDLHuxhDPlyFTY4gcFv6E4pLCtNjKIezNxmkszF7QT9Y5ZB1D3
+FB_PAGENAME=sk23design
+
+
+*/
+
+    $fbSecretKey = '31028bebf86d3785561d4982795fb9b5';
+    $fbAppId = '233236122274096';
+    $fbPageName = 'keycarleasing';
+
+    $response = fb_feed()->setAppId($fbAppId)->setSecretKey($fbSecretKey)->setPage($fbPageName)->feedLimit(12)->fetch();
+
+    return $response;
 });
